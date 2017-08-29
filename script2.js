92262f5d3346509ca2472b131f33c70f82062149
caiocaly <30990504+caiocaly@users.noreply.github.com>
2017-08-28 21:50:00 -0300

    Teste botões e objeto com propriedades

--- script2.js
+++ script2.js
@@ -1,18 +1,14 @@
-quarto = function (){
-	printContent(quarto, padrao, replace);
-}
-
-
-printContent = function (targetRoom, targetState, printMode){
-	document.getElementById('main').innerHTML = targetRoom.targetState;
-}
-
-testContent =function () {
-	//document.getElementById('teste').innerHTML = 'Oi';
-	 document.getElementById('main').innerHTML = quarto.padrao;
-}
-
 var quarto = {
 	padrao: "Testando isso",
 	apagado: "tudo escuro kkk"
 }
+
+printContent = function (targetRoom, targetState){
+	document.getElementById('main').innerHTML = targetRoom.targetState;
+}
+
+testContent = function () {
+	//document.getElementById('teste').innerHTML = 'Oi';
+	 document.getElementById('main').innerHTML = quarto.padrao;
+}
+
