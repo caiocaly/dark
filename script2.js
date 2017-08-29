740dd54cb0e7045f5080b3c497c5e0c1646befe3
caiocaly <30990504+caiocaly@users.noreply.github.com>
2017-08-28 22:38:05 -0300

    Teste 1 (quebrado)

--- script2.js
+++ script2.js
@@ -1,14 +1,18 @@
 var quarto = {
-	padrao: "Testando isso",
-	apagado: "tudo escuro kkk"
+	visitas: 0,
+	primeira: "Testando isso, bem vindo ao Quarto!",
+	seguintes: "É só o quarto",
+	decima: "sério, é só o quarto..."
+}
+
+quarto = function () {
+		printContent (quarto, 'primeira', 'add' );
 }
 
 printContent = function (targetRoom, targetState, printType){
 	if (printType == 'add') {
 
 	  document.getElementById('main').innerHTML += "<p>" + targetRoom[targetState] + "</p>";
-
-
 	
 	} else if (printType == 'replace') {
 	  document.getElementById('main').innerHTML = "<p>" + targetRoom[targetState] + "</p>";
